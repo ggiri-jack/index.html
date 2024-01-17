d49c2662d226c92b78b762e3640e6ecaf1c88448
jaeho <kms01226@gmail.com>
2024-01-17 23:48:14 +0900

    add : js

---
+++ JS Study/index1-1.js
@@ -0,0 +1,9 @@
+// let a = 1;
+// let b = 2;
+// ...
+
+let a = 1, b = 2, c = 3;
+const X = 4, Y = 5, Z = 6;
+
+console.log(a, b, c);
+console.log(X, Y, Z);
---
+++ JS Study/index1-2.js
@@ -0,0 +1,7 @@
+
+console.log(x, y);
+let x = 1;
+let y = x;
+x = 'Hello!';
+console.log('변경 후', x, y);
+console.log('변경 전', x, y);
